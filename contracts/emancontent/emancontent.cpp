49c9dce34fb38eb082bca56ac811779d8d0a9ca1
Miguel Tadeu <miguel.tadeu@oracle.com>
2018-06-14 23:37:29 +0100

    New additions to the content contract - Add/remove track - Get inserted tracks - Get tracks statistics - Statistics play counter and play duration

--- contracts/emancontent/emancontent.cpp
+++ contracts/emancontent/emancontent.cpp
@@ -3,25 +3,100 @@
  *  @copyright defined in eos/LICENSE.txt
  */
 #include "emancontent.hpp"
+#include <eosiolib/crypto.h>
+#include <string.h>
 
 namespace emanate
 {
-    
-void content::addtrack(account_name owner, const trackMetadata &metadata)
+
+uint64_t content::calculateChecksum(const std::string &str)
+{
+    uint64_t result = 0;
+    checksum256 checksum;
+    sha256(const_cast<char *>(str.c_str()), str.size(), &checksum);
+    memcpy(&result, &checksum, sizeof(result));
+    return result;
+}
+
+template<typename callback>
+void content::updateStats(account_name owner, const std::string &title, callback &&l)
+{
+    statTable stats( _self, owner );
+    uint64_t checksum = calculateChecksum(title);
+
+    auto iter = stats.find( checksum );
+    eosio_assert( iter != stats.end(), "could not find the statistics for this title" );
+
+    stats.modify( iter, owner, l);
+}
+
+void content::addtrack(account_name owner, const std::string &title, const std::string &metadata)
 {
     require_auth( owner );
-    
+
+    uint64_t checksum = calculateChecksum(title);
     trackTable tracks( _self, owner );
 
     tracks.emplace( owner, [&]( auto &track )
     {
         track.id = tracks.available_primary_key();
-        track.title = metadata.trackName;   //  extract from the json metadata
+        track.checksum = checksum;
+        track.title = title;
         track.metadata = metadata;
+    });
+
+    statTable stats( _self, owner );
+    stats.emplace( owner, [&] (auto &statRecord)
+    {
+        prints("Adding statistics");
+        printui(checksum);
+        statRecord.id = checksum;
+        statRecord.totalSecondsPlayed = 0;
+        statRecord.totalTimesPlayed = 0;
     });
 }
 
-EOSIO_ABI( emanate::content, (addtrack) )
+void content::removetrack(account_name owner, const std::string &title)
+{
+    require_auth( owner );
+
+    uint64_t checksum = calculateChecksum(title);
+    
+    {
+        trackTable tracks(_self, owner);
+        auto secondary_index = tracks.get_index<track::secondaryName>();
+        secondary_index.find(checksum);
+
+        auto iter = secondary_index.find(checksum);
+        eosio_assert(iter != secondary_index.end(), "track not found");
+
+        secondary_index.erase(iter);
+    }
+    {
+        statTable stats(_self, owner);
+        auto iter = stats.find(checksum);
+        eosio_assert(iter != stats.end(), "stats not found");
+        stats.erase(iter);
+    }
+}
+
+void content::startplaying(account_name owner, const std::string &title)
+{
+    updateStats(owner, title, [&]( auto &statRecord )
+    {
+        statRecord.totalTimesPlayed++;
+    });
+}
+
+void content::play(account_name owner, const std::string &title, uint64_t seconds)
+{
+    updateStats(owner, title, [&]( auto &statRecord )
+    {
+        statRecord.totalSecondsPlayed += seconds;
+    });
+}
+
+EOSIO_ABI( content, (addtrack)(removetrack)(startplaying)(play) )
 
 
 }   // namespace emanate
--- contracts/emancontent/emancontent.hpp
+++ contracts/emancontent/emancontent.hpp
@@ -5,33 +5,45 @@
 namespace emanate
 {
 
-struct trackMetadata
-{
-    std::string trackName;
-    std::string artistName;
-    
-    EOSLIB_SERIALIZE( trackMetadata, (trackName)(artistName) )
-};
-
 struct track
 {
     uint64_t id;                //  The id of this track
-    std::string title;
-    trackMetadata metadata;
+    uint64_t checksum;          //  Checksum for the title
+    std::string title;          //  Title of the track
+    std::string metadata;       //  Metadata for this track...this is a JSON string containing all the metadata
     
-    auto primary_key() const { return id; }
+    static const uint64_t secondaryName = N(bychecksum);
+    
+    uint64_t primary_key() const { return id; }
+    uint64_t get_secondary() const { return checksum; }
+
+    typedef eosio::indexed_by<track::secondaryName, eosio::const_mem_fun<track, uint64_t, &track::get_secondary>> SecondaryIndex;
 };
 
+struct stat {
+    uint64_t id;                    //  The track title checking will serve as an id
+    uint64_t totalSecondsPlayed;
+    uint64_t totalTimesPlayed;
+
+    uint64_t primary_key() const { return id; }
+};
 
 class content : public eosio::contract 
 {
-    typedef eosio::multi_index<N(track), track> trackTable;
-    
+    typedef eosio::multi_index<N(track), track, track::SecondaryIndex> trackTable;
+    typedef eosio::multi_index<N(stat), stat> statTable;
+
+    uint64_t calculateChecksum(const std::string &str);
+    template<typename callback> void updateStats(account_name owner, const std::string &title, callback &&l);
+
 public:
     
     content( account_name self ):contract(self){}
     
-    void addtrack(account_name owner, const trackMetadata &metadata);
+    void addtrack(account_name owner, const std::string &title, const std::string &metadata);
+    void removetrack(account_name owner, const std::string &title);
+    void startplaying(account_name owner, const std::string &title);
+    void play(account_name owner, const std::string &title, uint64_t seconds);
 };
     
 }   // namespace emanate
